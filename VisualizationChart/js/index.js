22738196b47575e6cea9061187b47acbf14dad65
Peach Zhang <CrazyCatZhang@outlook.com>
2022-03-13 21:33:43 +0800

    Create index.js

---
+++ VisualizationChart/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    (function () {
+        $('.monitor .tabs').on('click', 'a', function () {
+            $(this).addClass('active').siblings('a').removeClass('active');
+            $('.monitor .content').eq($(this).index()).show().siblings('.content').hide();
+        });
+    })();
+});
